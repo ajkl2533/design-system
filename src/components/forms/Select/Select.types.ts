67031d27dd6760d70db8cc13a36f6863a9355341
Radek Podrazky <rpodrazky@securityscorecard.io>
2021-09-30 14:19:06 +0200

    feat(Select): add Select component

--- src/components/forms/Select/Select.types.ts
+++ src/components/forms/Select/Select.types.ts
@@ -1,16 +1,51 @@
+import {
+  GroupedOptionsType,
+  MenuProps,
+  OptionsType,
+  Props as ReactSelectProps,
+} from 'react-select';
+import { Props as ReactSelectCreatableProps } from 'react-select/creatable';
+import PropTypes from 'prop-types';
+
+import { ActionKinds } from '../../../types/action.types';
+
 export interface Option {
   value: string;
   label: string;
+  isDisabled?: boolean;
 }
 
-export interface SelectProps {
-  options: Option[];
-  placeholder?: string;
-  isInvalid?: boolean;
-  isDisabled?: boolean;
-  isClearable?: boolean;
-  isMulti?: boolean;
-  defaultValue?: Option | Option[];
+interface BaseSelectProps<IsMulti extends boolean = false>
+  extends ReactSelectProps<Option, IsMulti> {
   defaultIsMenuOpen?: boolean;
-  className?: string;
+  menuActions?: Array<
+    ActionKinds<
+      [MenuProps<GroupedOptionsType<Option> | OptionsType<Option>, IsMulti>]
+    >
+  >;
+  maxPillLabelLength?: number;
+  isMenuPositionRelative?: boolean;
+  isInvalid?: boolean;
 }
+export type SelectProps<IsMulti extends boolean = false> = ReactSelectProps<
+  Option,
+  IsMulti
+> &
+  BaseSelectProps<IsMulti>;
+export type CreatableSelectProps<
+  IsMulti extends boolean = false
+> = ReactSelectCreatableProps<Option, IsMulti> &
+  BaseSelectProps<IsMulti> & {
+    createNewLabel?: string;
+  };
+
+export const OptionPropType = PropTypes.exact({
+  value: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  isDisabled: PropTypes.bool,
+});
+
+export const GroupPropType = PropTypes.exact({
+  label: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(OptionPropType).isRequired,
+});
